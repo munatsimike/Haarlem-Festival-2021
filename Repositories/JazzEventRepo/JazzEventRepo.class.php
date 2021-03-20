c1384aa34c054a0284a350232934eb53993bfdb2
FateofVoid <dshadowangels@Gmail.com>
2021-03-20 16:18:17 +0100

    first commit minor changes

--- Repositories/JazzEventRepo/JazzEventRepo.class.php
+++ Repositories/JazzEventRepo/JazzEventRepo.class.php
@@ -13,7 +13,8 @@
 		public function fetchJazzTickets() : array
 		{
 			if ($this->pdo instanceof PDO) {
-				return $this->pdo->query('SELECT id, price, date, start, end, artist, venue, seats FROM jazz_ticket')
+				return $this->pdo->query("	SELECT E.ID, E.price, E.date, E.start, E.end, artist, E.seats FROM jazz_ticket AS J 
+											INNER JOIN event AS E ON E.ID = J.event_id")
 					   ->fetchAll(PDO::FETCH_CLASS, 'JazzTicket');
 			}
 
