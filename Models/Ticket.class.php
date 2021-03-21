3ffa80e7c26750a3b5ffe0636e37a26e84fbc4a0
FateofVoid <dshadowangels@Gmail.com>
2021-03-21 20:33:20 +0100

    reworked class system

--- Models/Ticket.class.php
+++ Models/Ticket.class.php
@@ -7,7 +7,22 @@
 		public string $date;
 		public string $start = "";
 		public string $end = "";
-		public string $artist;
 		public string $venue;
+		public string $seats;
+
+
+		public function __construct(string $title, int $id, float $price , string $date, string $start, string $end, string $venue, string $seats) 
+		{ 
+			$this->title = $title;
+			$this->id = $id;
+			$this->price = $price;
+			$this->date = $date;
+			$this->start = $start;
+			$this->end = $end;
+			$this->venue = $venue;
+			$this->seats = $seats;
+		} 
+
+		abstract public function getTitle();
 	}
 ?>
