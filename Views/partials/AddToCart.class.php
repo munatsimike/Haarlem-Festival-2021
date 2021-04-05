77335cfde01bbd17b23943c18a0ac6ce4a296e0a
FateofVoid <dshadowangels@Gmail.com>
2021-04-05 16:31:28 +0200

    Revert "small changes"
    This reverts commit 6c8b7da3ce791e2b8c1efd4aa8bc0265a36c8a16.

--- Views/partials/AddToCart.class.php
+++ Views/partials/AddToCart.class.php
@@ -4,22 +4,12 @@ class AddToCart
 {
     public static function addToCart(Ticket $row) : void
     {
-        $quantity = 1;
-        $btnData = [];        
-        //display add to cart btn
-        if (in_array($row->ID, array_column(Cart::getCartItems(), 'id'))) {
-            $quantity = Cart::getCartItems()[array_search($row->ID, array_column(Cart::getCartItems(), 'id'))]->quantity;
-            $btnData = [ "added-to-cart-btn", "Added to Cart"];
-
-        } elseif ($row->price > 0) {
-            $btnData = ['add-to-cart-btn','Add to Cart'];
-        }
         // display ticket quantity option
         echo "<td width='4%'>";
                     if ($row->price > 0 ) {
             echo "<section>
                         <div class='value-button' class='decrease' onclick='decreaseValue()' value='Decrease Value'></div>
-                            <input type='number' class ='quantity' value='$quantity'/>
+                            <input type='number' class ='quantity' value='1'/>
                             Seats: <span class ='seats'>$row->seats</span>
                         <div class='value-button' class='increase' onclick='increaseValue()' value='Increase Value'></div>
                     </section>";
@@ -28,11 +18,16 @@ class AddToCart
                     }
                 "<div class ='input-group'>";
                     
-                if($btnData !== []){
-                echo "<button class ='" . $btnData[0] . "' type='button' name = 'cart-btn'>
-                    <span> <i class ='bi bi-cart4'style='font-size: 1.2rem'></i> </span> " . $btnData[1] . " </button>";
+                //display add to cart btn
+                if (in_array($row->ID, array_column(Cart::getCartItems(), 'id'))) {
+                echo  "<button class ='added-to-cart-btn' type='button' name = 'cart-btn'>
+                        <span> <i class ='bi bi-cart4'style='font-size: 1.2rem'></i> </span> Added to Cart</button>";
+
+                } elseif ($row->price > 0) {
+                    echo  "<button class ='add-to-cart-btn' type='button' name = 'cart-btn'>
+                            <span> <i class ='bi bi-cart4'style='font-size: 1.2rem'></i> </span> Add to Cart</button>";
                 }
-                "</div>
+            "</div>
             </td>";
     }
 }
